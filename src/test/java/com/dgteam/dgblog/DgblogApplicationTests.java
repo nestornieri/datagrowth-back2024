a99a2ace287f8cde3999efdf242269c687eb27c9
Nestor Nieri <nraulnieri@gmail.com>
2023-08-22 21:51:25 -0500

    Inicio Proyecto de Backend Data Growth

---
+++ src/test/java/com/dgteam/dgblog/DgblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgteam.dgblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
