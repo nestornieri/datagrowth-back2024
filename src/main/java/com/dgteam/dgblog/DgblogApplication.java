a329013427ddf8d6ae6f7184a64211daa51692a3
Nestor Nieri Torres <42565684+nestornieri@users.noreply.github.com>
2024-01-29 23:35:14 -0500

    añadir acceso a cors

--- src/main/java/com/dgteam/dgblog/DgblogApplication.java
+++ src/main/java/com/dgteam/dgblog/DgblogApplication.java
@@ -3,11 +3,28 @@ package com.dgteam.dgblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
 public class DgblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DgblogApplication.class, args);
 	}
+
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Permite todas las solicitudes desde cualquier origen
+        config.addAllowedMethod("*"); // Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+        config.addAllowedHeader("*"); // Permite todos los encabezados
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
